187ab8efa290033da34564ab90ecbeac378a8b60
kimlyndon <codeculture101@gmail.com>
2018-11-20 18:45:56 -0500

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Kim Lyndon on 11/20/18.
+//  Copyright © 2018 Kim Lyndon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
