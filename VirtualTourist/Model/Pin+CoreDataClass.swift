8165f2fdd870f5b84d6233eaf4a72e77dd400688
kimlyndon <codeculture101@gmail.com>
2018-12-15 00:57:28 -0500

    Add: Code the DataClass and DataProperties(Pin)

--- VirtualTourist/Model/Pin+CoreDataClass.swift
+++ VirtualTourist/Model/Pin+CoreDataClass.swift
@@ -10,4 +10,17 @@ import Foundation
 import CoreData
 
 @objc(Pin)
-
+public class Pin: NSManagedObject {
+    convenience init(longitude: Double, latitude: Double, context: NSManagedObjectContext) {
+        
+        if let ent = NSEntityDescription,entity(forEntityName: "Pin", in: context) {
+            self.init(entity: ent, insertInto: context)
+            self.latitude = latitude
+            self.longitude = longitude
+            
+        } else {
+            
+            fatalError("Unable to find Pin in entities")
+        }
+    }
+}
--- VirtualTourist/Model/Pin+CoreDataProperties.swift
+++ VirtualTourist/Model/Pin+CoreDataProperties.swift
@@ -8,3 +8,34 @@
 
 import Foundation
 import CoreData
+
+
+extension Pin {
+    
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Pin> {
+        return NSFetchRequest<Pin>(entityName: "Pin")
+    }
+    
+    @NSManaged public var latitude: Double
+    @NSManaged public var longitude: Double
+    @NSManaged public var photos: NSSet?
+    
+}
+
+// MARK: Accessors for photos
+extension Pin {
+    
+    @objc(addPinObject:)
+    @NSManaged public func addToPhotos(_ value: Photo)
+    
+    @objc(addPinObject:)
+    @NSManaged public func removeFromPhotos(_ value: Photo)
+    
+    @objc(addPinObject:)
+    @NSManaged public func addToPhotos(_ value: NSSet)
+    
+    @objc(addPinObject:)
+    @NSManaged public func removeFromPhotos(_ value: NSSet)
+    
+    
+}
