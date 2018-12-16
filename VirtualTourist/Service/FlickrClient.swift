a8030abba061bef66ee20310fcc106e8fb65ccbb
kimlyndon <codeculture101@gmail.com>
2018-12-15 22:22:56 -0500

    Add: guard statements for api requests

--- VirtualTourist/Service/FlickrClient.swift
+++ VirtualTourist/Service/FlickrClient.swift
@@ -79,7 +79,74 @@ class FlickrClient: NSObject {
     }
     func convertURLToPhotoData(photURL: String, completionHandler: @escaping(_ photoData: Data?, _ error: NSError?) -> Void) -> URLSessionTask {
         
+        let url = URL(string: photoURL)
+        let request = URLRequest(url: url!)
         
-        }
+        let task = session.dataTask(with: request) { data, response, downloadError in
+            
+            if downloadError != nil {
+                //Cancel task
+            } else {
+                completionHandler(data, nil)
+            }
      }
+        
+        task.resume()
+        return task
+}
+
+//Abstract the guard statements for requests to one location
+    private func performRequest(request: URLRequest, completionHandler: @escaping (_ results: AnyObject?, _ error: NSError?) -> Void) -> URLSessionDataTask {
+        
+        let task = session.dataTask(with: request as URLRequest) { data, response, error in
+            
+            func sendError(_ error: String) {
+                print(error)
+                let userInfo = [NSLocalizedDescriptionKey : error] completionHandlerRequest(nil, NSError?(domain: "performRequest", code: 1, userInfo: userInfo))
+            }
+            
+            guard (error == nil) else {
+                sendError("There was an error with your request: \(error!)")
+                return
+            }
+            
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                let httpError = (response as? HTTPURLResponse)?.statusCode
+                sendError("Your request returned a status code other than 2xx")
+                return
+            }
+            
+            guard let data = data else {
+                sendError("No data was returned by the request")
+                return
+            }
+            
+            self.convertDataWithCompletionHandler(data, completionHandlerForConvertData: completionHandlerRequest)
+        }
+        
+        task.resume()
+        return task
+}
+
+    // When given raw JSON, return a usable Foundation object
+    private func convertDataWithCompletionHandler(_ data: Data, completionHandlerForConvertData: (_ result: AnyObject?, _ error: NSError?) -> Void) {
+        var parsedResult: AnyObject! = nil
+        do {
+            parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as AnyObject
+        } catch {
+            let userInfo = [NSLocalizedDescriptionKey : "Could not parse the data as JSON: '\(data)'"]
+            completionHandlerForConvertData(nil, NSError(domain: "convertDataWithCompletionHandler", code: 1, userInfo: userInfo))
+        }
+        
+        completionHandlerForConvertData(parsedResult, nil)
+    }
+    
+    // MARK: Shared Instance
+    
+    class func sharedInstance() -> FlickrClient {
+        struct Singleton {
+            static var sharedInstance = FlickrClient()
+        }
+        return Singleton.sharedInstance
+    }
 }
