7041a79e29a5fb9052c239d7a30c64615cdb65fa
kimlyndon <codeculture101@gmail.com>
2018-12-16 18:03:35 -0500

    Fix: Re-Add Pin+CoreDataClass/Properties files

---
+++ VirtualTourist/Model/Pin+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Pin+CoreDataClass.swift
+//  VirtualTourist
+//
+//  Created by Kim Lyndon on 12/16/18.
+//  Copyright © 2018 Kim Lyndon. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Pin)
+public class Pin: NSManagedObject {
+    
+}
