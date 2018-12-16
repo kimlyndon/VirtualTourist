6cbee3d2bff42c15fb829a3a085dee97532baab3
kimlyndon <codeculture101@gmail.com>
2018-12-15 22:33:06 -0500

    Add: code collectionButtonPressed

--- VirtualTourist/Controllers/PhotoAlbumViewController.swift
+++ VirtualTourist/Controllers/PhotoAlbumViewController.swift
@@ -12,5 +12,63 @@ import CoreData
 
 class PhotoAlbumController: UIViewController {
 
-
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionButton: UIBarButtonItem!
+    
+    var pin: Pin!
+    var photos: [Photo] = [Photo]()
+    var dataController: dataController!
+    var fetchedResultsController: NSFetchedResultsController<Photo>!
+    //Indices of photos for fetchedResultsController
+    var photoIndicesToInsert = [IndexPath]()
+    var photoIndicesToDelete = [IndexPath]()
+    var photoIndicesToUpdate = [IndexPath]()
+    var selectedPhotoIndices = [IndexPath]()
+    //Determine if collection button is enabled
+    var numberOfDownloadedImages: Int = 0
+    //Total number of pages returned by Flickr call for pin
+    var numberOfPagesForPin: Int = 0
+    var currentPageNumber: Int = 1
+    
+    //MARK: Actions
+    @IBAction func collectionButtonPressed(_ sender: UIBarButtonItem) {
+        //Remove all photos
+        if sender.title == Constants.BarButtonTitles.NewCollection {
+            collectionButton.isEnabled = false
+            for photo in fetchedResultsController.fetchedObjects! {
+                dataController.viewContext.delete(photo)
+            }
+            
+            dataController.save()
+            //Update current page number of photo results from Flickr
+            if currentPageNumber < numberOfPagesForPin {
+                currentPageNumber += 1
+                
+            } else {
+                
+                currentPageNumber = numberOfPagesForPin
+            }
+            
+            collectionView.isHidden = false
+            
+            //Get all new photos
+            retrievePhotosFromFlickr(forPage: currentPageNumber)
+            
+        } else {
+        //Remove only selected photos
+            for photoIndex in selectedPhotoIndices {
+                let photo = fetchedResultsController.object.(at: photoIndex)
+                dataController.viewContext.delete(photo)
+            }
+            
+            dataController.save()
+            //Reset selected indices
+            selectedPhotoIndices.removeAll()
+            collectionButton.title = Constants.BarButtonTitles.NewCollection
+        }
+        
+        
+    }
+    
 }
