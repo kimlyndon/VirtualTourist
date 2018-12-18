f8abe18c64ab4bf994225cc34f2bfc60759991af
kimlyndon <codeculture101@gmail.com>
2018-12-17 19:21:58 -0500

    Edit: move code from Pin to Photo

--- VirtualTourist/Model/Pin+CoreDataProperties.swift
+++ VirtualTourist/Model/Pin+CoreDataProperties.swift
@@ -23,19 +23,3 @@ extension Pin {
     
 }
 
-// MARK: Generated accessors for photos
-extension Pin {
-    
-    @objc(addPhotosObject:)
-    @NSManaged public func addToPhotos(_ value: Photo)
-    
-    @objc(removePhotosObject:)
-    @NSManaged public func removeFromPhotos(_ value: Photo)
-    
-    @objc(addPhotos:)
-    @NSManaged public func addToPhotos(_ values: NSSet)
-    
-    @objc(removePhotos:)
-    @NSManaged public func removeFromPhotos(_ values: NSSet)
-    
-}
